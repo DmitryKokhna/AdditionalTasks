8c02a9e5a76c85d69a1b1dceaa7d4f1d794a37bb
DmitryKokhna <dmitry.kokhna@gmail.com>
2024-10-30 23:34:21 +0300

    30_Oct_2024 -1 comments

--- src/P1/classOne.java
+++ src/P1/classOne.java
@@ -1,2 +1,4 @@
+package P1;
+
 public class classOne {
 }
---
+++ src/P2/comments.java
@@ -0,0 +1,14 @@
+package P2;
+
+public class comments {
+    public static void main(String[] args) {
+        System.out.println("Hello world3 !");
+        /**
+         *
+         * new doc comment
+         */
+        // 1str comment
+        /* 2sts comment
+  *
+         */
+    }}
