fc1c5c9535706cffce75795726a84ffb0a63bf19
DmitryKokhna <dmitry.kokhna@gmail.com>
2024-11-01 00:04:14 +0300

    31_Oct_2024 -1 comments

--- src/P2/comments.java
+++ src/P2/comments.java
@@ -2,10 +2,17 @@ package P2;
 
 public class comments {
     public static void main(String[] args) {
+        int a = 1;
         System.out.println("Hello world3 !");
+        /*-
+        test
+
+
+         */
         /**
          *
          * new doc comment
+         * @param a - first
          */
         // 1str comment
         /* 2sts comment
