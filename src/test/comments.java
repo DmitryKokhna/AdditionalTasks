9e682076dfb87f0f0e93342a5fe12913efbf93ee
DmitryKokhna <dmitry.kokhna@gmail.com>
2024-11-02 23:52:25 +0300

    02_Nov_2024 -1 int-String-int

---
+++ src/test/comments.java
@@ -0,0 +1,10 @@
+package test;
+
+public class comments {
+    public static void main(String[] args) {
+        int a = 1;
+
+String b = "" + a;
+    int c = Integer.parseInt(b);
+System.out.println(c);
+    }}
