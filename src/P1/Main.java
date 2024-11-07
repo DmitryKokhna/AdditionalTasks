032b367ee02dd646269a81d318453b271c37e2fa
DmitryKokhna <dmitry.kokhna@gmail.com>
2024-11-07 18:54:21 +0300

    07_Nov_2024 -1 class Scanner and while / if

--- src/P1/Main.java
+++ src/P1/Main.java
@@ -1,7 +1,29 @@
 package P1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world !");
+//String a = "a'b'c";
+
+
+        Scanner scan = new Scanner(System.in);
+     //   String a = scan.next();
+
+        scan.useDelimiter(" ");
+String s;
+
+if (scan.hasNext()) {
+    while (scan.hasNext()) {
+        s = scan.next();
+        System.out.println(s);
+    }
+
+    }
+else{
+    scan.close();
+}
+
+
     }
 }
