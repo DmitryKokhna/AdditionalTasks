368f9d67d30c1f237c4fec1e443763147c28637a
DmitryKokhna <dmitry.kokhna@gmail.com>
2024-11-24 20:08:03 +0300

    24_Nov_2024 -1

---
+++ src/P161124/cl1.java
@@ -0,0 +1,24 @@
+package P161124;
+
+import java.util.Scanner;
+
+public class cl1 {
+    public static void main(String[] arg) {
+
+   /*     String s1 = "Привет";
+        String s2 = "ПРИВЕТ";
+        String s3 = s1.toUpperCase();
+
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equals(s3));
+        System.out.println(s2.equals(s3));
+        System.out.println("v2"+ s1.equalsIgnoreCase(s2));
+        System.out.println(s1.equalsIgnoreCase(s3));
+        System.out.println(s2.equals(s3));
+*/
+        String a =""+1;
+        String b =""+2;
+        String s = String.join("-","sfsf",b,a);
+        System.out.println(s);
+    }
+}
---
+++ src/while_c_s/while_c_s.java
@@ -0,0 +1,22 @@
+package while_c_s;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.SequenceInputStream;
+import java.util.Scanner;
+
+public class while_c_s {
+    public static void main(String[] arg) {
+        int n = 5;
+        while (n > 0) {
+            System.out.println(n);
+            --n;
+
+            Scanner scanner = new Scanner(System.in);
+            int a= scanner.nextInt();
+
+
+        }
+
+    }
+}
