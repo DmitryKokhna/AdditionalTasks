87e8e3c6975a891ef4dfbbcff8bb958c8f6a90b4
DmitryKokhna <dmitry.kokhna@gmail.com>
2024-11-26 19:36:35 +0300

    26_Nov_2024 задачи на WHILE

--- src/P161124/cl1.java
+++ src/P161124/cl1.java
@@ -5,20 +5,24 @@ import java.util.Scanner;
 public class cl1 {
     public static void main(String[] arg) {
 
-   /*     String s1 = "Привет";
-        String s2 = "ПРИВЕТ";
-        String s3 = s1.toUpperCase();
+        //напишите тут ваш код
 
-        System.out.println(s1.equals(s2));
-        System.out.println(s1.equals(s3));
-        System.out.println(s2.equals(s3));
-        System.out.println("v2"+ s1.equalsIgnoreCase(s2));
-        System.out.println(s1.equalsIgnoreCase(s3));
-        System.out.println(s2.equals(s3));
-*/
-        String a =""+1;
-        String b =""+2;
-        String s = String.join("-","sfsf",b,a);
-        System.out.println(s);
+        Scanner console = new Scanner(System.in);
+
+        int max = Integer.MIN_VALUE;
+        while (console.hasNextInt()) {
+            int x = console.nextInt();
+            if (x % 2 == 0 && x > max) {
+                max = x;
+            }
+        }
+        System.out.println(max);
     }
 }
+
+
+
+
+
+
+
--- src/while_c_s/while_c_s.java
+++ src/while_c_s/while_c_s.java
@@ -7,16 +7,63 @@ import java.util.Scanner;
 
 public class while_c_s {
     public static void main(String[] arg) {
-        int n = 5;
-        while (n > 0) {
-            System.out.println(n);
-            --n;
 
-            Scanner scanner = new Scanner(System.in);
-            int a= scanner.nextInt();
+        //напишите тут ваш код
+     /*   int stringCount = 0;
 
+        while (stringCount < 10) {
+            int columnCount = 0;
 
+            while (columnCount < 20) {
+                if ((stringCount == 0 || stringCount == 9) && (columnCount == 0 || columnCount == 19)) {
+                    System.out.print("Б");
+                }
+                    else
+                    {
+                        System.out.print(" ");
+                    }
+                    columnCount++;
+                }
+                System.out.println();
+                stringCount++;
+
+            }
+*/
+
+    /*    //Задача №1:
+        //Необходимо вывести на экран числа от 1 до 7. То есть на экране должно быть: 1 2 3 4 5 6 7
+        int i=1;
+        while (i<8){
+            System.out.println(i);
+            i++;
         }
+
+
+        // Задача №2:
+        //  Необходимо вывести на экран вот такую последовательность чисел: 100 90 80 70 60 50 40 30 20 10
+        //вар_1 решение
+
+     */
+        int i = 100;
+        while (i > 1){
+            System.out.println(i);
+            i-=10;
+        }
+
+// вар_2 решение
+     /*   int i = 100;
+        while (i > 1) {
+            if (i % 10 == 0) {
+                System.out.println(i);
+            }
+            i--;
+        }
+*/
 
     }
 }
+
+
+
+
+
