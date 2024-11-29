aee95f512a1e3feb4f9753945503fffe09966bc9
DmitryKokhna <dmitry.kokhna@gmail.com>
2024-11-29 12:40:12 +0300

    29_Nov_2024 - задачи 2 для DoWhile

--- src/CycleDoWhile/cycledowhile.java
+++ src/CycleDoWhile/cycledowhile.java
@@ -5,7 +5,8 @@ import java.util.Scanner;
 
 public class cycledowhile {
     public static void main(String[] arg){
-// ЗАДАЧА ИЗ JAVA RUSH
+        /*
+// ЗАДАЧА 1 ИЗ JAVA RUSH
         Scanner scanner = new Scanner(System.in);
         String string = scanner.nextLine();
         int number = scanner.nextInt();
@@ -14,6 +15,19 @@ public class cycledowhile {
             number--;
         }
         while (number > 0 && number < 4);
+
+        */
+
+        //ЗАДАЧА 2
+        //Распечатать все числа от 1 до 100, которые делятся на 5 без остатка.
+        int i=1;
+        do{
+            if(i%5 ==0){
+                System.out.println(i);
+            }
+           i++;
+        } while(i<=100);
+
     }
 
 }
