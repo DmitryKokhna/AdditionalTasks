3a204b9dc861d240d609efc75c4c62d79ad289f2
DmitryKokhna <dmitry.kokhna@gmail.com>
2024-12-19 19:41:02 +0300

    19_Dec_2024 - arrayDouble Задача_2

--- src/ArrayDouble/ArrayDouble.java
+++ src/ArrayDouble/ArrayDouble.java
@@ -21,6 +21,28 @@ public class ArrayDouble {
         }
 */
 
+        // ЗАДАЧА_2
+        //  Создать массив типа String размером 3х6.
+        //  И записать в него значения:
+        //   a1  a2  a3  a4  a5  a6
+        //  b1  b2  b3  b4  b5  b6
+        //   c1  c2   c3  c4  c5  c6
+        // Распечатать массив.
+
+        String[][] array2 = new String[3][6];
+        for (int i = 0; i < array2.length; i++) {
+            for (int j = 0; j < array2[i].length; j++) {
+                if (i == 0) {
+                    array2[i][j] = "a" + (j + 1);
+                } else if (i == 1) {
+                    array2[i][j] = "b" + (j + 1);
+                } else {
+                    array2[i][j] = "c" + (j + 1);
+                }
+                System.out.print(array2[i][j] + " ");
+            }
+            System.out.println();
+        }
 
 
     }
