f313d43979a428e76d2f664641281fa177e173fb
DmitryKokhna <dmitry.kokhna@gmail.com>
2024-12-19 19:28:56 +0300

    19_Dec_2024 - arrayDouble

--- src/ArrayDouble/ArrayDouble.java
+++ src/ArrayDouble/ArrayDouble.java
@@ -1,19 +1,28 @@
 package ArrayDouble;
 
+import java.util.Arrays;
+
 public class ArrayDouble {
-    public static int[][] MULTIPLICATION_TABLE;
 
     public static void main(String[] args) {
-        //напишите тут ваш код
+
+/*
+        //ЗАДАЧА_1
+        int[][] MULTIPLICATION_TABLE;
         MULTIPLICATION_TABLE = new int[10][10];
 
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                MULTIPLICATION_TABLE[i][j] = (i+1)*(j+1);
-                System.out.print(MULTIPLICATION_TABLE[i][j]+ " ");
+                MULTIPLICATION_TABLE[i][j] = (i + 1) * (j + 1);
+                System.out.print(MULTIPLICATION_TABLE[i][j] + " ");
             }
             System.out.println();
             int a = MULTIPLICATION_TABLE[0].length;
         }
+*/
+
+
+
     }
+
 }
